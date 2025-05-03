6477ebd36b917bf323625544da717a67780fc008
ExoPexodus <rushilrana@gmail.com>
2025-05-03 12:33:06 +0530

    fixed the exec format error

--- backend/run.sh
+++ backend/run.sh
@@ -1,15 +1,6 @@
 
 #!/bin/bash
 
-# Wait for postgres to be ready
-echo "Waiting for Postgres to be ready..."
-until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
-  echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
-echo "Postgres is up - running migrations"
-
 # Run migrations
 cd /app
 alembic upgrade head
